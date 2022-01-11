71a190f1cb41d3d5c411891498413d7591ac17cb
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-01-10 19:13:40 -0800

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 
 /*Assign Button Element Object Representation*/
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 /*Function to create Task Item*/
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  };
+
+};
 
 /*Create & Style New Tas Item*/
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
